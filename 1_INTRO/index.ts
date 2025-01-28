233ffa4babeeaeb934bf1539b1b6a3fc929215e7
Mauricio Amaral <mauricioamaral.trabalho@gmail.com>
2025-01-28 16:32:53 -0300

    Intalação do express

--- 1_INTRO/index.ts
+++
@@ -1,9 +0,0 @@
-const firstName = "Mauricio"
-const anotherName = 1
-const x = true
-
-function greeting(name) {
-    console.log("Olá "+ name)
-}
-
-greeting(firstName)
---
+++ express-com-TS/src/app.ts
@@ -0,0 +1,15 @@
+// 1 - iniciando projeto 
+//console.log("Express + TS!!!")
+
+//init express
+import express from "express";
+
+const app = express()
+
+app.get("/", (req, res) =>{
+    return res.send("Hello, World!")
+})
+
+app.listen(3000, () => {
+    console.log("Aplicação de TS + Express funcionado!")
+}) 
